959b42658e5e8bee5abf225ea47d0490713cd1dd
vitoriacurty <vitoria.curty@hotmail.com>
2023-03-04 17:56:40 -0300

    Create components react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./assets/logo.png"
+
+export default function App() {
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" />
+      <h1>ZapRecall</h1>
+    </div>
+  );
+}
+
